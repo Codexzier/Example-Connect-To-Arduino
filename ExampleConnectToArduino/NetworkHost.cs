27e745c544e45a5386a396eac88a748d3ca34c90
Codexzier <johannes.paul.langner@live.de>
2017-10-02 18:52:17 +0200

    Completing the sample code and adding the arduino code to receive the commands.

---
+++ ExampleConnectToArduino/NetworkHost.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ExampleConnectToArduino
+{
+    public class NetworkHost
+    {
+        private Socket _connection;
+        private int _port = 1200;
+
+        /// <summary>
+        /// Standard constructor.
+        /// </summary>
+        /// <param name="port">Set the port for the network connection.</param>
+        public NetworkHost(int port)
+        {
+            this._port = port;
+        }
+
+        /// <summary>
+        /// Starts the socket class with the configurate as server.
+        /// </summary>
+        public void Start()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, this._port));
+            socket.Listen(1);
+
+            this._connection = socket.Accept();
+        }
+        
+        /// <summary>
+        /// Send a command to 
+        /// </summary>
+        /// <param name="command">letter can not be blank</param>
+        public void SendCommand(char command)
+        {
+            if (!char.Equals(command, ' '))
+            {
+                this._connection.Send(Encoding.UTF8.GetBytes(new char[] { command }));
+            }
+        }
+
+        /// <summary>
+        /// Disposed and stop the socket.
+        /// </summary>
+        public void Stop()
+        {
+            if (this._connection != null)
+            {
+                this._connection.Dispose();
+                this._connection = null;
+            }
+        }
+    }
+}
--- ExampleConnectToArduino/Program.cs
+++ ExampleConnectToArduino/Program.cs
@@ -10,6 +10,29 @@ namespace ExampleConnectToArduino
     {
         static void Main(string[] args)
         {
+            NetworkHost host = new NetworkHost(1200);
+            host.Start();
+
+            bool run = true;
+            while (run)
+            {
+                Console.WriteLine("a = ON");
+                Console.WriteLine("b = OFF");
+                Console.WriteLine("c = Close application");
+
+                string enter = Console.ReadLine();
+
+                if(enter.Contains("a") || enter.Contains("b"))
+                {
+                    host.SendCommand(enter.ToCharArray()[0]);
+                }
+                else if(enter == "c")
+                {
+                    run = false;
+                }
+            }
+
+            host.Stop();
         }
     }
 }
